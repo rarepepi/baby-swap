37d77cd0b92ab81f5305b6cdb6133a4eff92a67e
rarepepi <pepimartinez@protonmail.com>
2024-06-11 11:31:49 -0400

    uniswap widget has errors

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center">
       <MainPage />
-      <UniSwapPage />
+      {/* <UniSwapPage /> */}
     </main>
   );
 }
