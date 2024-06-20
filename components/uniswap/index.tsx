9a20b2bdc9561df690b4077ca0b112ba9ab82975
rarepepi <pepimartinez@protonmail.com>
2024-06-20 14:16:54 -0400

    more fixes

--- components/uniswap/index.tsx
+++ components/uniswap/index.tsx
@@ -10,9 +10,17 @@ export const UniSwapPage = () => {
       name: "Baby Token",
       address: "0x59E25CC1E5a9AbDA2593debD74A5215836A10C9a",
       symbol: "BABY",
-      decimals: 18,
+      decimals: 9,
       chainId: 8453,
       logoURI: "https://buy.getbaby.xyz/logo.webp",
+    },
+    {
+      name: "USDC",
+      address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+      symbol: "USDC",
+      decimals: 6,
+      chainId: 8453,
+      logoURI: "https://basescan.org/token/images/centre-usdc_28.png",
     },
   ];
 
@@ -30,13 +38,13 @@ export const UniSwapPage = () => {
         settings={{
           slippage: {
             auto: false,
-            max: "13.00",
+            max: "14.00",
           },
-          routerPreference: RouterPreference.CLIENT,
+          routerPreference: RouterPreference.API,
           transactionTtl: 60 * 20,
         }}
-        convenienceFee={1}
-        convenienceFeeRecipient={"0x2184d6a4fbd26584eb4ff90a215c1db334209053"}
+        // convenienceFee={1}
+        // convenienceFeeRecipient={"0x2184d6a4fbd26584eb4ff90a215c1db334209053"}
         defaultOutputTokenAddress={"0x59E25CC1E5a9AbDA2593debD74A5215836A10C9a"}
         // defaultChainId={8453}
       />
